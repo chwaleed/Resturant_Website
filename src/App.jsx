9b9782ebbb567b56584b40d3b537298bb2ba7fb5
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-04-28 12:59:26 +0500

    footer page is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import { useState } from "react";
-import Button1 from "./compnents/button1";
-import Button2 from "./compnents/button2";
-import ServicesCard from "./compnents/servicesCard";
-import { FaHamburger } from "react-icons/fa";
-import GalleryCard from "./compnents/galleryCard";
-import MenuCard from "./compnents/menuCard";
-import menu1 from "./assets/Images/menu-01.jpg";
-import TeamCard from "./compnents/teamCard";
 import Navbar from "./compnents/navbar";
 import Hero from "./pages/Hero/Hero";
 import Aboutus from "./pages/About Us/Aboutus";
@@ -18,6 +10,7 @@ import Team from "./pages/Team/Team";
 import Testimonials from "./pages/Testimonials/Testimonials";
 import BlogCard from "./compnents/blogCard";
 import Blogs from "./pages/Blogs/Blogs";
+import Footer from "./pages/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -34,42 +27,7 @@ function App() {
       <Team />
       <Testimonials />
       <Blogs />
-      {/* <>
-        <h1 className="text-[2rem] font-bold text-secondary font-heading">
-          Hello kese ho
-        </h1>
-        <Button1
-          size={"text-[1.1rem]"}
-          text={"Book Now"}
-          bg={"bg-primary"}
-          x={"px-7"}
-          y={"py-2"}
-        />
-        <Button2
-          size={"text-[1.1rem]"}
-          bg={"bg-light"}
-          text={"Book Now"}
-          x={"px-5"}
-          y={"py-1"}
-        />
-        <ServicesCard
-          title={"Our Services"}
-          dic={"Contrary to popular belief, ipsum is not simply random."}
-          logo={
-            <FaHamburger className="text-[8rem] group-hover:text-dark  transition duration-[0.7s] text-primary" />
-          }
-        />
-        <GalleryCard />
-        <MenuCard
-          img={menu1}
-          price={"90$"}
-          title={"Panner"}
-          text={
-            "Consectetur adipiscing elit sed dwso eiusmod tempor incididunt ut labore."
-          }
-        />
-        <TeamCard name={"Henry"} text={"Decoration Chef"} />
-      </> */}
+      <Footer />
     </>
   );
 }
---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import Button1 from "../../compnents/button1";
+import { FaCheck } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoCall } from "react-icons/io5";
+import { IoMdMail } from "react-icons/io";
+import { IoTime } from "react-icons/io5";
+import { FaCopyright } from "react-icons/fa6";
+import menu1 from "../../assets/Images/menu-01.jpg";
+import menu2 from "../../assets/Images/menu-02.jpg";
+import menu3 from "../../assets/Images/menu-03.jpg";
+import menu4 from "../../assets/Images/menu-04.jpg";
+import menu5 from "../../assets/Images/menu-05.jpg";
+import menu6 from "../../assets/Images/menu-06.jpg";
+
+function Footer() {
+  return (
+    <div className="mt-[15rem] font-body">
+      <div className="bg-light grid lg:grid-cols-4  md:grid-cols-2 px-[10%] md:px-[8%] lg:px-[5%] py-16 gap-6">
+        <div className="flex flex-col gap-5">
+          <h1 className="font-heading text-[2.7rem] md:text-[2.8rem] font-semibold">
+            <span className="text-primary">Cater</span>Sera
+          </h1>
+          <p className="leading-9 w-[90%] text-[1.1rem] text-secondary">
+            There cursus massa at urnaaculis estieSed aliquamellus vitae ultrs
+            condmentum leo massamollis its estiegittis miristum.
+          </p>
+          <div className="flex gap-3   mt-2">
+            <Button1
+              size={"text-[1.1rem]"}
+              text={<FaFacebookF className="text-[1.3rem]" />}
+              bg={"bg-primary"}
+              x={
+                "flex justify-center items-center w-[2.8rem] h-[2.8rem] rounded-[50%]"
+              }
+            />
+            <Button1
+              size={"text-[1.1rem]"}
+              text={<FaTwitter className="text-[1.3rem]" />}
+              bg={"bg-primary"}
+              x={
+                "flex justify-center items-center w-[2.8rem] h-[2.8rem] rounded-[50%]"
+              }
+            />
+            <Button1
+              size={"text-[1.1rem]"}
+              text={<FaInstagram className="text-[1.3rem]" />}
+              bg={"bg-primary"}
+              x={
+                "flex justify-center items-center w-[2.8rem] h-[2.8rem] rounded-[50%]"
+              }
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <h1 className="text-[1.8rem]  font-semibold">Special Facilities</h1>
+          <ul className="flex text-secondary flex-col gap-5 text-[1.1rem]">
+            <li className="flex  items-center gap-2">
+              <FaCheck className="text-primary text-[1.1rem]" /> Cheese Burger
+            </li>
+            <li className="flex  items-center gap-2">
+              <FaCheck className="text-primary text-[1.1rem]" /> Sandwich
+            </li>
+            <li className="flex  items-center gap-2">
+              <FaCheck className="text-primary text-[1.1rem]" /> Panner Burger
+            </li>
+            <li className="flex  items-center gap-2">
+              <FaCheck className="text-primary text-[1.1rem]" /> Special Sweet
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-5">
+          <h1 className="text-[1.8rem] font-semibold">Contact Us</h1>
+          <ul className="flex text-secondary flex-col gap-5 text-[1.1rem]">
+            <li className="flex items-center gap-2">
+              <FaLocationDot className="text-primary text-[1.1rem]" /> 123
+              Street, New York, USA
+            </li>
+            <li className="flex items-center gap-2">
+              <IoCall className="text-primary text-[1.1rem]" /> (+012) 3456 7890
+              123
+            </li>
+            <li className="flex items-center gap-2">
+              <IoMdMail className="text-primary text-[1.1rem]" />{" "}
+              info@example.com
+            </li>
+            <li className="flex items-center gap-2">
+              <IoTime className="text-primary text-[1.1rem]" /> 26/7 Hours
+              Service
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-5">
+          <h1 className="text-[1.8rem] font-semibold">Social Gallery</h1>
+          <div className="grid grid-cols-3 gap-2">
+            <img
+              src={menu1}
+              className="rounded-[50%] w-[7rem] border-[1px] border-primary p-2"
+              alt=""
+            />
+            <img
+              src={menu2}
+              className="rounded-[50%] w-[7rem] border-[1px] border-primary p-2"
+              alt=""
+            />
+            <img
+              src={menu3}
+              className="rounded-[50%] w-[7rem] border-[1px] border-primary p-2"
+              alt=""
+            />
+            <img
+              src={menu4}
+              className="rounded-[50%] w-[7rem] border-[1px] border-primary p-2"
+              alt=""
+            />
+            <img
+              src={menu5}
+              className="rounded-[50%] w-[7rem] border-[1px] border-primary p-2"
+              alt=""
+            />
+            <img
+              src={menu6}
+              className="rounded-[50%] w-[7rem] border-[1px] border-primary p-2"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <div className="bg-dark">
+        <h1>
+          <FaCopyright />{" "}
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
